a78d533528572aed07c5951649cf8230318e03f1
codarsssss <codarsssss@gmail.com>
2023-07-18 00:07:09 +0300

    create fastapi object and necessary

---
+++ app.py
@@ -0,0 +1,7 @@
+import fastapi
+import database
+import pydantic_models
+import config
+
+
+api = fastapi.FastAPI()
--- main.py
+++
@@ -1,7 +0,0 @@
-import fastapi
-
-api = fastapi.FastAPI()
-
-@api.get('/hello')
-def api_hello():
-    return {"hello":'from api!'}
