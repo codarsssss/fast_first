0806602934e164e2b5694e76712134e4429a767b
codarsssss <codarsssss@gmail.com>
2023-07-18 01:23:20 +0300

    exercising get_3 hard path

--- app.py
+++ app.py
@@ -36,5 +36,11 @@ def get_id2(id: int):         # либо же его можно задавать
 
 @api.get('/user_id_str/{id:str}')
 def get_id2(id):
-    return {"user":id}
+    return {"user":id}     # тут id - это уже строка, так как мы объявили тип данных
 
+
+@api.get('/test/{id:int}/{text:str}/{custom_path:path}')
+def get_test(id, text, custom_path):
+    return {"id":id,
+            "":text,
+            "custom_path": custom_path}
