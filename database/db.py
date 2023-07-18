4aeb8e01b20e9b20b5fa368c9b3f7c8d8194b852
codarsssss <codarsssss@gmail.com>
2023-07-18 03:36:49 +0300

    create entity

--- database/db.py
+++ database/db.py
@@ -1,1 +1,18 @@
 import pony
+from pony.orm import *
+import models
+
+db = Database()
+
+
+class User(db.Entity):
+    user_id = Required(str)
+    nick = Required(str)
+    age = Required(int)
+    wallets = Set('Wallet')
+
+
+class Wallet(db.Entity):
+    address = Required(str)
+    private_key = Required(str)
+    owner = Required(User)
